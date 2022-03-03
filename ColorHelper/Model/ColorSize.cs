f4a64ce341833042cf3a8709dc80a9f03f75ee16
239573049 <239573049@qq.com>
2022-03-04 03:55:51 +0800

    修改

---
+++ ColorHelper/Model/ColorSize.cs
@@ -0,0 +1,26 @@
+﻿namespace ColorHelper.Model
+{
+    public class ColorSize
+    {
+        public short X { get; set; }
+        public short Y { get; set; }
+        /// <summary>
+        /// 数据是否为空
+        /// </summary>
+        public bool IsNull { get; set; } = true;
+        public ColorSize() =>
+            IsNull = true;
+        public ColorSize(short x, short y)
+        {
+            X = x;
+            Y = y;
+            IsNull = false;
+        }
+        public ColorSize(ColorSize size)
+        {
+            X = size.X;
+            Y = size.Y;
+            IsNull = false;
+        }
+    }
+}
